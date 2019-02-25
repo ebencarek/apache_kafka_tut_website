83ddeb2cb1c545c43f1a6f06e2d4ac732248753b
mattlangsenkamp <mattlangsenkamp@yahoo.com>
2019-02-25 17:50:40 -0500

    added bootstrap for UI

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 #@app.route('/about')
-def about_page():
-    return render_template('about.html')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/Theory')
+def theory_page():
+    return render_template('theory.html')
+
+@app.route('/Quickstart')
+def quickstart_page():
+    return render_template('quick_start.html')
+
+@app.route('/Download')
+def download_page():
+    return render_template('download.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
